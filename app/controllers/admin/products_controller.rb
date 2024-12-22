0ee464fe2239faa73de6375a2902d53fe70ae37c
Henrique Alexandre de Camargo <henriquealexandredec@gmail.com>
2024-12-22 10:57:05 -0300

    :repairing:
    Signed-off-by: Henrique Alexandre de Camargo <henriquealexandredec@gmail.com>

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,50 @@
 class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: %i[edit update destroy]
+  before_action :set_categories, only: %i[new edit]
+
   def index
+    @products = Product.includes(:category).all
   end
 
   def new
+    @product = Product.new
   end
 
-  def edit
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: 'Product created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: 'Product updated successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: 'Product deleted successfully.'
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_categories
+    @categories = Category.all
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :image)
   end
 end
--- app/controllers/admin/variants_controller.rb
+++ app/controllers/admin/variants_controller.rb
@@ -1,10 +1,50 @@
 class Admin::VariantsController < ApplicationController
+  before_action :set_variant, only: %i[edit update destroy]
+  before_action :set_products, only: %i[new edit]
+
   def index
+    @variants = Variant.includes(:product).all
   end
 
   def new
+    @variant = Variant.new
   end
 
-  def edit
+  def create
+    @variant = Variant.new(variant_params)
+    if @variant.save
+      redirect_to admin_variants_path, notice: 'Variant created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @variant.update(variant_params)
+      redirect_to admin_variants_path, notice: 'Variant updated successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @variant.destroy
+    redirect_to admin_variants_path, notice: 'Variant deleted successfully.'
+  end
+
+  private
+
+  def set_variant
+    @variant = Variant.find(params[:id])
+  end
+
+  def set_products
+    @products = Product.all
+  end
+
+  def variant_params
+    params.require(:variant).permit(:name, :price, :product_id)
   end
 end
